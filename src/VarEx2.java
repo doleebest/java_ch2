4ded782c2300a89a190c986642a7e979b1a23f81
So Jeong Lee <sj27419@gmail.com>
2022-01-13 01:01:46 +0900

    first commit

---
+++ src/VarEx2.java
@@ -0,0 +1,16 @@
+import javax.net.ssl.CertPathTrustManagerParameters;
+
+public class VarEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 4, y=2;
+		int temp;
+		temp = x;
+		x=y;
+		y=temp;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+	}
+
+}
