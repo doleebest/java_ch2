b4e4c0478ad8d748218185e24dcbaed2bc6c8eee
So Jeong Lee <sj27419@gmail.com>
2022-01-13 23:45:45 +0900

    first commit

--- src/VarEx2.java
+++ src/VarEx2.java
@@ -4,13 +4,35 @@ public class VarEx2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 4, y=2;
-		int temp;
-		temp = x;
-		x=y;
-		y=temp;
-		System.out.println("x="+x);
-		System.out.println("y="+y);
+		System.out.println(10/3);
+		System.out.println(10.0/3);
+		System.out.printf("%d\n",15);
+		System.out.printf("%#o\n",15);
+		System.out.printf("%#x\n",15);
+		System.out.printf("%s\n",Integer.toBinaryString(15));
+
+		double f=123.456789;
+		System.out.printf("%f\n",f); //float은 7자리까지만 정확
+		System.out.printf("%e\n",f); //float은 7자리까지만 정확
+		System.out.printf("%g\n",f); //float은 7자리까지만 정확
+									// double은 9자리까지 정확
+		
+		System.out.println();
+		System.out.printf("[%5d]\n",10);
+		System.out.printf("[%-5d]\n",10); //왼쪽정렬
+		System.out.printf("[%05d]\n",10); //왼쪽정렬
+		System.out.printf("[%05d]\n",1234567); //지정된자리>원래자리 -> 걍원래대로출력
+		
+		System.out.println();
+		double d = 1.23456789;
+		
+		System.out.printf("%f\n",d);
+		System.out.printf("%14.10f",d);
+		
+		System.out.println();
+		System.out.printf("[%s]\n","www.codechobo.com");
+		System.out.printf("[%20s]\n","www.codechobo.com");
+		System.out.printf("[%.10s]\n","www.codechobo.com"); //.+숫자 = 일부만 출력가능	
 	}
 
 }
---
+++ src/VarEx4.java
@@ -0,0 +1,38 @@
+
+public class VarEx4 {
+
+	public static void main(String[] args) {
+	 final int score = 100;
+	 boolean power = false;
+	 
+	 byte b = 127; // byte : -128~127
+	 int oct = 010; // 8진수 10진수로 8
+	 int hex = 0x10; // 16진수, 10진수로 16
+	 System.out.println(oct);
+	 System.out.println(hex);
+	 System.out.println(power);
+	 
+	 //long l = 1000_000_000; // int의 범위 < long 범위
+	 long l = 10_000_000_000L; // int는 ~20000000000(20억)
+	 
+	 float f = 3.14f; // f생략 불가
+	 double d = 3.14f; // f 써도 ok 왜냐면 double>float
+	
+	 
+	 System.out.println(10.);
+	 System.out.println(.10);
+	 System.out.println(10f);
+	 System.out.println(1e3);
+	 
+	 char ch = 'A';
+	 int i = 'a'; //코드 번호 출력
+	 String string = "";
+	 String str2 = "ABCD";
+	 String str3 = "123";
+	 String str4 = str2+str3;
+	 System.out.println(str4);
+	 System.out.println(i);
+	 
+	}
+
+}
